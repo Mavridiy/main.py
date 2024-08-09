93a9b09bc2f2946ee410e7b3eee7c2d07242147e
Mavridiy <161286685+Mavridiy@users.noreply.github.com>
2024-08-09 21:32:46 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd program
+x = 2*2+2
+y = 2*(2+2)
+print(x)
+print(y)
+print(x==y)
+
+#4th program
+num = 123.456
+print(int(num*10%10))
+
